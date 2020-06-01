859fcfec89f6f27a9f5c3f729d9d279b7a62b41b
Joshua Carew <joshua.carew@yahoo.com>
2020-06-01 13:22:59 +0100

    Updates

---
+++ precache-manifest.6048c923ecb4f8c350ef65b8c10187c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0886491689010ff56eac6e505cec4298",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6c67da671a97875b4da5",
+    "url": "/weather-app/static/css/main.5ac243b5.chunk.css"
+  },
+  {
+    "revision": "c36515d594ba73ebb6a7",
+    "url": "/weather-app/static/js/2.5e7b9b10.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-app/static/js/2.5e7b9b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c67da671a97875b4da5",
+    "url": "/weather-app/static/js/main.0885c330.chunk.js"
+  },
+  {
+    "revision": "2d005965a0cd2533529c",
+    "url": "/weather-app/static/js/runtime-main.e2ed4ade.js"
+  }
+]);
